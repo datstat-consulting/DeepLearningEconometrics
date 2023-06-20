e65e8df1d10393f84783439f70e6d0f505b007b0
Datstat Consulting <85887371+datstat-consulting@users.noreply.github.com>
2023-06-20 20:19:17 +0800

    fixed problems with add_bias

--- SignalProcessing.py
+++ SignalProcessing.py
@@ -1,15 +1,17 @@
 import torch.nn as nn
 
+import torch.nn as nn
+
 class KalmanNet:
-    def __init__(self, layer_sizes_f, layer_sizes_h, activation_function, optimizer_function):
+    def __init__(self, layer_sizes_f, layer_sizes_h, activation_function, optimizer_function, add_bias = True):
         self.layer_sizes_f = layer_sizes_f
         self.layer_sizes_h = layer_sizes_h
         self.activation_function = activation_function
         self.optimizer_function = optimizer_function
         # state transition model
-        self.model_f = PerceptronMain(self.layer_sizes_f, self.activation_function, self.optimizer_function, add_bias=False)
+        self.model_f = PerceptronMain(self.layer_sizes_f, self.activation_function, self.optimizer_function, add_bias=add_bias)
         # observation model
-        self.model_h = PerceptronMain(self.layer_sizes_h, self.activation_function, self.optimizer_function, add_bias=False)
+        self.model_h = PerceptronMain(self.layer_sizes_h, self.activation_function, self.optimizer_function, add_bias=add_bias)
         # RNN for learning the Kalman Gain
         self.rnn = nn.RNN(input_size=1, hidden_size=1, batch_first=True)
 
@@ -20,20 +22,25 @@ class KalmanNet:
         self.model_f.fit(X, y, epochs, batch_size, learning_rate)
         self.model_h.fit(X, y, epochs, batch_size, learning_rate)
 
-    def predict_next_state(self, current_state):
+    def predict_next_state(self, current_state, add_bias = True):
         """
         Predict the next state given the current state.
         Uses the state transition model f.
         """
+        if add_bias == True:
+            current_state = torch.cat((current_state, torch.tensor([1.0])))
         next_state_pred = self.model_f.forward(current_state)
         return next_state_pred
 
-    def update_state(self, next_state_pred, observation):
+    def update_state(self, next_state_pred, observation, add_bias = True):
         """
         Update the predicted next state with the new observation to
         compute the corrected next state estimate.
         Uses the Kalman Gain to correct the prediction.
         """
+        if add_bias:
+            next_state_pred = torch.cat((next_state_pred, torch.tensor([1.0])))
+        
         # Compute the innovation term
         innovation = observation - self.model_h.forward(next_state_pred)
         
